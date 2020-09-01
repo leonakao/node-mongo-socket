8efcb0f150f188469cc5689f1b4d57764855da24
Leonardo Nakao <leonakao.dev@gmail.com>
2020-08-31 21:44:11 -0300

    feat: create a namespaced 'chat'

--- src/services/chat/index.ts
+++ src/services/chat/index.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 import socketio, { Socket } from 'socket.io'
 import http from 'http'
 import { SocketAuthenticationError } from '../../errors'
@@ -12,7 +13,9 @@ export default {
       origins: allowedOrigins,
     })
 
-    io.use((socket: Socket, next: Function) => {
+    const ChatManager = io.of('/chat')
+
+    ChatManager.use((socket: Socket, next: Function) => {
       const token = socket.handshake.query.Authorization || null
       if (token === 'token') {
         return next()
@@ -20,16 +23,15 @@ export default {
       return next(new SocketAuthenticationError('Invalid Token'))
     })
 
-    io.on('connection', socket => {
-      // eslint-disable-next-line no-console
-      console.log(`User connected: ${socket.id}`)
+    ChatManager.on('connection', user => {
+      console.log(`User connected: ${user.id}`)
 
-      socket.on('disconnect', reason => {
-        console.log(`User disconnected: ${socket.id}`)
+      user.on('disconnect', reason => {
+        console.log(`User disconnected: ${user.id}`)
         console.log(`Reason: ${reason}`)
       })
 
-      socket.on('error', error => {
+      user.on('error', error => {
         console.log(`User error: ${error}`)
       })
     })
