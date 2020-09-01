016be2c6245d65cba028423e73868460ace66db0
Leonardo Nakao <leonakao.dev@gmail.com>
2020-08-31 21:43:55 -0300

    refactor: remove name

--- src/errors/SocketAuthenticationError.ts
+++ src/errors/SocketAuthenticationError.ts
@@ -1,13 +1,10 @@
 import { DataSocketError } from './protocols'
 
 export class SocketAuthenticationError extends Error {
-  name: string
-
   data: DataSocketError
 
   constructor(reason: string) {
     super('Invalid authorization')
-    this.name = 'Invalid authorization'
     this.data = {
       type: 'authorization_error',
       message: reason,
